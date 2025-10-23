489f8197536a40d547d3c214ab0b12c295909381
Leonard Rwai Kanyesigye <50195658+rmauritsson@users.noreply.github.com>
2025-10-23 07:20:29 +0300

    Added default fonts and adjsted the root route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,34 @@
+import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
-import { AuthProvider } from "./context/AuthContext";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 import { toastConfig } from "./components/ui/ToastConfig";
+import { AuthProvider } from "./context/AuthContext";
 import { KYCProvider } from "./context/KYCContext";
 import { ShipmentProvider } from "./context/ShipmentContext";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
+    'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
+    'Inter-SemiBold': require('../assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Italic': require('../assets/fonts/Inter-Italic.ttf'),
+    'SpaceMono-Regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  
   return (
     <AuthProvider>
       <KYCProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
       colors: {
         primary: '#163330',
         secondary: '#CDFF00',
-      }
+      },
+      fontFamily: {
+        sans: ['Inter-Regular'],
+        bold: ['Inter-Bold'],
+        semibold: ['Inter-SemiBold'],
+        italic: ['Inter-Italic'],
+        mono: ['SpaceMono-Regular'],
+      },
     },
   },
   plugins: [],
